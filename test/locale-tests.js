ebd08fb2c742cc778282ac5595182a14e6cd831d
Bundit <whytobe@gmail.com>
2016-04-27 16:37:05 +0700

    Add localization test runner

--- test/locale-tests.js
+++ test/locale-tests.js
@@ -95,6 +95,7 @@ QUnit.test('Localization files can be loaded and applied', function(assert) {
     assert.equal(ko.validation.locale('ro-RO'), 'ro-ro', 'Romanian - Romania (ro-RO)');
     assert.equal(ko.validation.locale('ru-RU'), 'ru-ru', 'Russian - Russia (ru-RU)');
     assert.equal(ko.validation.locale('sv-SE'), 'sv-se', 'Swedish - Sweden (sv-SE)');
+    assert.equal(ko.validation.locale('th-TH'), 'th-th', 'Thai - Thai (th-TH)');
     assert.equal(ko.validation.locale('tr-TR'), 'tr-tr', 'Turkish - Turkey (tr-TR)');
     assert.equal(ko.validation.locale('zh-CN'), 'zh-cn', 'Chinese - China (zh-CN)');
     assert.equal(ko.validation.locale('zh-TW'), 'zh-tw', 'Chinese - Taiwan (zh-TW)');
